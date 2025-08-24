ffbbe520db8e58080fcdbfdc3ba2c93a107c7737
DESKTOP-73HTUT7\Marci <aaarby@gmail.com>
2025-08-24 14:46:22 +0200

    relative ffmpeg dirs, build script

---
+++ build_windows.sh
@@ -0,0 +1,14 @@
+git submodule update --init
+if (-Not (Test-Path -Path vcpkg )) { New-Item -ItemType Directory -Path vcpkg }
+cd vcpkg
+git init
+git remote add origin https://github.com/microsoft/vcpkg.git
+git fetch origin
+git checkout -b master --track origin/master
+git checkout b27651341123a59f7187b42ef2bc476284afb310
+.\bootstrap-vcpkg.bat
+.\vcpkg integrate install
+.\vcpkg install libusb libpcap libsodium ffmpeg qt5 sdl2 vcpkg-tool-ninja
+cd ..
+cmake "-DCMAKE_TOOLCHAIN_FILE=C:/Dev/fpv4win_fork/fpv4win/vcpkg/scripts/buildsystems/vcpkg.cmake" -S ./ -B "build" -G "Visual Studio 17 2022"
+cmake --build build --config Release --target fpv4win
