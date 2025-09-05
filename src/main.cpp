6e8dd3437f5039b55c053437bd5f1de6e1967dfe
arby <arbocz.marton@gmail.com>
2025-09-06 01:27:03 +0200

    simplify code

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "player/FrameSocketSender.h"
 
 #include <chrono>
+#include <memory>
 
 #pragma comment(lib, "ws2_32.lib")
 #define HEADLESS_MODE TRUE
@@ -37,58 +38,46 @@ LONG ApplicationCrashHandler(EXCEPTION_POINTERS *pException) {
 
 #endif
 
-QString url = "sdp/sdp.sdp";
-std::thread analysisThread;
 bool playStop = false;
 shared_ptr<FFmpegDecoder> decoder;
-std::mutex mtx;
-FrameSocketSender* frameSender = new FrameSocketSender();
-volatile bool isMuted = true;
 
 std::chrono::steady_clock::time_point last_frame_send = std::chrono::steady_clock::now();
 
-void play(const int image_send_frequency_ms) {
-    analysisThread = std::thread([image_send_frequency_ms]() {
-        auto decoder_ = make_shared<FFmpegDecoder>();
-
-        std::string asd = url.toStdString();
-        bool ok = decoder_->OpenInput(asd);
+void start_decode_thread(const int image_send_frequency_ms) {
+    std::thread decodeThread([image_send_frequency_ms]() {
+        std::unique_ptr<FrameSocketSender> frameSender = std::make_unique<FrameSocketSender>();
+        auto decoder = make_shared<FFmpegDecoder>();
+    
+        std::string url = "sdp/sdp.sdp";
+        bool ok = decoder->OpenInput(url);
         if (!ok) {
             std::cout << "error\n";
             return;
         }
-        {
-            std::lock_guard<std::mutex> lck(mtx);
-            decoder = decoder_;
-        }
-
-        std::thread decodeThread([image_send_frequency_ms]() {
-            while (!playStop) {
-                try {
-                    auto frame = decoder->GetNextFrame();
-                    if (!frame) {
-                        continue;
-                    }
-                    if (!frameSender->isConnected()) {
-                        frameSender->initialize();
-                    }
-
-                    std::chrono::steady_clock::time_point current_time = std::chrono::steady_clock::now();
-                    if (std::chrono::duration_cast<std::chrono::milliseconds>(current_time - last_frame_send).count() > image_send_frequency_ms) {
-                       frameSender->sendFrame(frame);
-                       last_frame_send = std::chrono::steady_clock::now();
-                    }
+        while (!playStop) {
+            try {
+                auto frame = decoder->GetNextFrame();
+                if (!frame) {
+                    continue;
                 }
-                catch (const exception& e) {
-                    std::cout << e.what();
-                    break;
+                if (!frameSender->isConnected()) {
+                    frameSender->initialize();
+                }
+    
+                std::chrono::steady_clock::time_point current_time = std::chrono::steady_clock::now();
+                if (std::chrono::duration_cast<std::chrono::milliseconds>(current_time - last_frame_send).count() > image_send_frequency_ms) {
+                   frameSender->sendFrame(frame);
+                   last_frame_send = std::chrono::steady_clock::now();
                 }
             }
-            playStop = true;
-            });
-        decodeThread.detach();
-        });
-    analysisThread.detach();
+            catch (const exception& e) {
+                std::cout << e.what();
+                break;
+            }
+        }
+        playStop = true;
+    });
+    decodeThread.detach();
 }
 
 void initialize_acquisition(const int image_send_frequency_ms) {
@@ -108,7 +97,7 @@ void initialize_acquisition(const int image_send_frequency_ms) {
     WFBReceiver::Instance().Start(vidPid.toStdString(), channel, channelWidth, keyPath.toStdString());
 
     QObject::connect(&QmlNativeAPI::Instance(), &QmlNativeAPI::onRtpStream, [image_send_frequency_ms]() {
-        play(image_send_frequency_ms);
+        start_decode_thread(image_send_frequency_ms);
     });
     while (!playStop) {
         sleep(0.1);
